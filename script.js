4024682b31fb6a3739f97bed37b6e9afd5cf7389
anmol25e <159610433+anmol25e@users.noreply.github.com>
2025-03-20 21:37:53 -0600

    new repo with all the existing code of the previous repo

---
+++ script.js
@@ -0,0 +1,61 @@
+
+let container = document.getElementById("container")
+let showUnbtn = document.getElementById("show-unshow")
+let unshowbtn = document.getElementById("unshow")
+let bgColour = document.getElementById("change-bg")
+
+
+function changeColorFromPicker() {
+    const selectedColor = document.getElementById("colorPicker").value;
+    document.getElementById("container").style.backgroundColor = selectedColor;
+}
+
+
+
+
+let gridSize = prompt("num?")
+function createBoard() {
+    let num = parseInt(gridSize)
+    container.innerHTML = "";
+    const containerSize = 400;
+    const colCal =num
+    const rowCal = num
+    const itemSize = containerSize/ colCal
+
+    for(let a=0; a<num * num; a++) {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add('gridItem')
+        gridItem.style.width = itemSize + 'px';
+        gridItem.style.height = itemSize + 'px';
+        gridItem.style.boxSizing = 'border-box';
+
+        showUnbtn.addEventListener('click', function () {
+            gridItem.style.border = '1px solid #ccc';
+            
+        })
+
+        unshowbtn.addEventListener('click', function () {
+            gridItem.style.border = 'none';
+        })
+
+
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.style.backgroundColor = 'black';
+        });
+
+
+        container.appendChild(gridItem);
+
+        console.log(`Rows: ${rowCal}, Columns: ${colCal}`);
+
+    }
+
+    
+}
+createBoard()
+
+
+bgColour.addEventListener('click', function () {
+    document.getElementById("colorPicker").style.display = "block"
+    
+})
